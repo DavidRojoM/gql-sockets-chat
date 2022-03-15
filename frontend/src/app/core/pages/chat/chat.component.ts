f9262f6714b1db39749a6ecdfb85e944a15f9846
David Rojo <davidrojo1997@gmail.com>
2022-03-15 20:04:24 +0100

    feat(frontend): add ChatComponent

---
+++ frontend/src/app/core/pages/chat/chat.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../../shared/services/local-storage/local-storage.service';
+import { Router } from '@angular/router';
+import { PostsService } from '../../shared/services/posts/posts.service';
+import { Observable } from 'rxjs';
+import { PostDto } from '../../domain/dto/post.dto';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss'],
+})
+export class ChatComponent implements OnInit {
+  public name = localStorage.getItem('name') as string;
+  public posts$!: Observable<PostDto[]>;
+
+  public form = this.fb.group({
+    content: ['', [Validators.required, Validators.minLength(3)]],
+  });
+  constructor(
+    private readonly localStorageService: LocalStorageService,
+    private readonly router: Router,
+    private readonly postsService: PostsService,
+    private readonly fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.posts$ = this.postsService.findAll();
+  }
+
+  logout() {
+    this.localStorageService.remove('name');
+    this.router.navigateByUrl('/login');
+  }
+
+  createPost(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      const { content } = this.form.value;
+      this.postsService.createOne({
+        id: '',
+        content,
+        title: this.name,
+      });
+      this.form.reset();
+    }
+  }
+}
