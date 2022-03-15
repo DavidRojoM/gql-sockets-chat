fbd2990a1b784867b13893b1db893cd5772c4647
David Rojo <davidrojo1997@gmail.com>
2022-03-15 16:30:41 +0100

    feat(backend): config GraphQLModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PostsModule } from './core/domain/posts/posts.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    PostsModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      installSubscriptionHandlers: true,
+    }),
+  ],
+  controllers: [],
 })
 export class AppModule {}
