21149470e53e12d8b640bdc2c6014eb0a0a6bc37
David Rojo <davidrojo1997@gmail.com>
2022-03-15 16:29:44 +0100

    feat(backend): add posts repository

---
+++ backend/src/core/domain/posts/posts.repository.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { Post } from './entities/post.entity';
+import { PostInput } from './dto/post.input';
+
+@Injectable()
+export class PostsRepository {
+  public posts: Post[] = [
+    {
+      id: '1',
+      title: 'title',
+      content: 'content',
+    },
+  ];
+
+  public findAll() {
+    return [...this.posts];
+  }
+
+  public addOne(post: PostInput) {
+    this.posts = [...this.posts, post];
+    return post;
+  }
+}
