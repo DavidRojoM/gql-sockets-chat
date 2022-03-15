2f28590179ce7f1e20c1c5b662a1504c0c37b702
David Rojo <davidrojo1997@gmail.com>
2022-03-15 22:05:10 +0100

    feat(frontend): add PostsService ngrx impl

--- frontend/src/app/core/shared/services/posts/posts.service.ts
+++ frontend/src/app/core/shared/services/posts/posts.service.ts
@@ -2,30 +2,35 @@ import { Injectable } from '@angular/core';
 import { PostsGqlRepository } from './gql-repository/posts-gql.repository';
 import { PostDto } from '../../../domain/dto/post.dto';
 import { v4 } from 'uuid';
+import { StoreService } from '../store/store.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostsService {
-  constructor(private readonly postsRepository: PostsGqlRepository) {
+  constructor(
+    private readonly postsRepository: PostsGqlRepository,
+    private readonly storeService: StoreService
+  ) {
     this.postAdded();
   }
 
+  public loadPosts() {
+    this.storeService.loadPosts();
+  }
+
   public findAll() {
-    return this.postsRepository.findAll();
+    return this.storeService.selectPosts();
   }
 
   public createOne(post: PostDto) {
     post.id = v4();
-
-    return this.postsRepository.addOne(post).subscribe((data) => {
-      console.log(data);
-    });
+    return this.postsRepository.addOne(post);
   }
 
-  public postAdded() {
-    this.postsRepository.postAdded().subscribe((data) => {
-      console.log('subs', data);
+  private postAdded() {
+    this.postsRepository.postAdded().subscribe((post) => {
+      this.storeService.loadPost(post);
     });
   }
 }
