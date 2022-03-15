3189e2bdf22fc7edcfda28094a02fe0e2b0399cc
David Rojo <davidrojo1997@gmail.com>
2022-03-15 22:01:54 +0100

    feat(frontend): add Action Types

---
+++ frontend/src/app/state/actions/action.types.enum.ts
@@ -0,0 +1,13 @@
+export enum ActionTypes {
+  LOAD_POSTS_REQUEST = '[Post] Load Posts Request',
+  LOAD_POSTS_SUCCESS = '[Post] Load Posts Success',
+  LOAD_POSTS_FAILURE = '[Post] Load Posts Failure',
+
+  LOAD_POST_REQUEST = '[Post] Load Post Request',
+  LOAD_POST_SUCCESS = '[Post] Load Post Success',
+  LOAD_POST_FAILURE = '[Post] Load Post Failure',
+
+  CREATE_POST_REQUEST = '[Post] Create Post Request',
+  CREATE_POST_SUCCESS = '[Post] Create Post Success',
+  CREATE_POST_FAILURE = '[Post] Create Post Failure',
+}
