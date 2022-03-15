b5b9689115bc660fcf504603a7483caba37fb715
David Rojo <davidrojo1997@gmail.com>
2022-03-15 20:04:44 +0100

    feat(frontend): add routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './core/pages/chat/chat.component';
+import { LoginComponent } from './core/pages/login/login.component';
+import { UnauthenticatedGuard } from './core/shared/guards/unauthenticated.guard';
+import { AuthenticatedGuard } from './core/shared/guards/authenticated.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    canActivate: [UnauthenticatedGuard],
+  },
+  {
+    path: 'chat',
+    component: ChatComponent,
+    canActivate: [AuthenticatedGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './core/pages/chat/chat.component';
+import { LoginComponent } from './core/pages/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { GraphQLModule } from './graphql.module';
+import { PostsService } from './core/shared/services/posts/posts.service';
+import { PostsGqlRepository } from './core/shared/services/posts/gql-repository/posts-gql.repository';
+import { ApolloModule } from 'apollo-angular';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ChatComponent, LoginComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    GraphQLModule,
+    ApolloModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [PostsService, PostsGqlRepository],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
